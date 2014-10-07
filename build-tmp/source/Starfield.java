89a71fcfeee63d1c9659544f97ba937e3fba1b6b
ben441318936 <ben441318936@gmail.com>
2014-10-06 20:02:45 -0700

    basics 2

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 int num=200;
 NormalParticle storm[]=new NormalParticle[num];
-void setup()
+public void setup()
 {
 	size(500,500);
 	background(0);
@@ -10,7 +26,7 @@ void setup()
 		storm[i]=new NormalParticle(250,250);
 	}
 }
-void draw()
+public void draw()
 {
 	//background(0);
 	noStroke();
@@ -25,7 +41,7 @@ void draw()
 class NormalParticle
 {
 	double xPosition,yPosition,angle,speed;
-	color c;
+	int c;
 	NormalParticle(int x, int y)
 	{
 		xPosition=x;
@@ -34,12 +50,12 @@ class NormalParticle
 		speed=1;
 		c= color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
 	}
-	void move()
+	public void move()
 	{
 		xPosition=Math.cos(angle)*speed+xPosition;
 		yPosition=Math.sin(angle)*speed+yPosition;
 	}
-	void show()
+	public void show()
 	{
 		noStroke();
 		fill(c);
@@ -53,4 +69,13 @@ interface Particle
 class OddballParticle
 {
 	//your code here
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
