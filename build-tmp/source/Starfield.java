e2e67bee826f8958a498235810eba29ebed4ff04
ben441318936 <ben441318936@gmail.com>
2014-10-16 20:49:58 -0700

    fun 6.2 implement preload

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 
 public class Starfield extends PApplet {
 
+/*@pjs preload="att-park.jpg";*/
 PImage img;
 public static int num=100;
 Particle firework1[]=new Particle[num+1];
